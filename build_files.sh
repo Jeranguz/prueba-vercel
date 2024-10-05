a1077ab76c2df56b79156c767f9b21799f28d94f
Jeranguz <JeremyGD997@hotmail.com>
2024-10-04 18:52:20 -0600

    prueba 2

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
-pip install -r requirements.txt
-py manage.py collectstatic
+# Instala las dependencias del proyecto
+python3 -m pip install -r requirements.txt
+
+# Ejecuta migraciones de la base de datos
+# Recopila los archivos estáticos
+python3 manage.py collectstatic --noinput
